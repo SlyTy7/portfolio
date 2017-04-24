90d6283689b900c2e8a1537107d0374b2590a368
slyty7 <tyler.j.west.7@gmail.com>
2017-04-23 18:34:02 -0700

    new projects object

---
+++ js/projects.js
@@ -0,0 +1,47 @@
+var projects = {
+	clock : {
+		title: 'CSS Clock',
+		info: popUp(),
+		link: 'projects/clock/index.html'
+	},
+	drums : {
+		title: 'Pure Javascript Drum Kit',
+		info: popUp(),
+		link: 'projects/drums/index.html'
+	},
+	voice : {
+		title: 'The Voiceinator 5000',
+		info: popUp(),
+		link: 'projects/voiceinator/index.html'
+	},
+	ahead : {
+		title: 'Type Ahead',
+		info: popUp(),
+		link: 'projects/ahead/index.html'
+	},
+	whack : {
+		title: 'Whack a Mole',
+		info: popUp(),
+		link: 'projects/mole/index.html'
+	},
+	flex : {
+		title: 'Flex Panels',
+		info: popUp(),
+		link: 'projects/flex/index.html'
+	},
+	wiki : {
+		title: 'title',
+		info: popUp(),
+		link: 'projects/palceholder.html'
+	},
+	quotes : {
+		title: 'title',
+		info: popUp(),
+		link: 'projects/palceholder.html'
+	},
+	weather : {
+		title: 'title',
+		info: popUp(),
+		link: 'projects/palceholder.html'
+	},
+}
